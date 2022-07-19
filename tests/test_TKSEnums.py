cd8d2d60b0cd905c13c447981b466a1bff5b2c45
Timur Gilmullin <tim55667757@gmail.com>
2022-07-19 22:11:52 +0300

    #4 Tests were added: "Checks constants types" and "Available instruments"

--- tests/test_TKSEnums.py
+++ tests/test_TKSEnums.py
@@ -11,30 +11,36 @@ class TestConstantsConsistent:
 
     @pytest.fixture(scope="function", autouse=True)
     def init(self):
-        pass
+        self.mainConstants = {
+            "TKS_INSTRUMENTS": "<class 'list'>",
+            "TKS_TICKER_ALIASES": "<class 'dict'>",
+            "TKS_TICKERS_OR_FIGI_EXCLUDED": "<class 'list'>",
+            "TKS_TIMEFRAMES": "<class 'dict'>",
+            "TKS_TRADING_STATUSES": "<class 'dict'>",
+            "TKS_OPERATION_TYPES": "<class 'dict'>",
+            "TKS_OPERATION_STATES": "<class 'dict'>",
+            "TKS_ORDER_DIRECTIONS": "<class 'dict'>",
+            "TKS_STOP_ORDER_DIRECTIONS": "<class 'dict'>",
+            "TKS_ORDER_TYPES": "<class 'dict'>",
+            "TKS_STOP_ORDER_TYPES": "<class 'dict'>",
+            "TKS_ORDER_STATES": "<class 'dict'>",
+            "TKS_STOP_ORDER_EXPIRATION_TYPES": "<class 'dict'>",
+        }
+        self.mainConstantsNames = self.mainConstants.keys()
+        self.currentConstantsNames = dir(TKSEnums)  # constants in fact
 
     def test_ConsistentOfConstantsNames(self):
-        mainConstantsNames = [
-            "TKS_INSTRUMENTS",
-            "TKS_OPERATION_STATES",
-            "TKS_OPERATION_TYPES",
-            "TKS_ORDER_DIRECTIONS",
-            "TKS_ORDER_STATES",
-            "TKS_ORDER_TYPES",
-            "TKS_STOP_ORDER_DIRECTIONS",
-            "TKS_STOP_ORDER_EXPIRATION_TYPES",
-            "TKS_STOP_ORDER_TYPES",
-            "TKS_TICKERS_OR_FIGI_EXCLUDED",
-            "TKS_TICKER_ALIASES",
-            "TKS_TIMEFRAMES",
-            "TKS_TRADING_STATUSES",
-        ]
-        currentConstantsNames = list(dir(TKSEnums))
-        for cName in mainConstantsNames:
-            assert cName in currentConstantsNames, "Attention! One of main constants changed it's name! Before: [{}], but now it is not in constants list.".format(cName)
+        for cName in self.mainConstantsNames:
+            assert cName in self.currentConstantsNames, "One of main constants changed it's name! Problem is with [{}]".format(cName)
+
+    def test_ConstantsTypes(self):
+        for cName in self.currentConstantsNames:
+            if cName in self.mainConstantsNames:
+                actualType = str(type(getattr(TKSEnums, cName)))
+                assert actualType == self.mainConstants[cName], "Data types mismatch! Constant [{}] must have type [{}], but current is [{}].".format(cName, self.mainConstants[cName], actualType)
 
     def test_AvailableInstruments(self):
         mainInstruments = ["Currencies", "Shares", "Bonds", "Etfs", "Futures"]
         currentInstruments = TKSEnums.TKS_INSTRUMENTS
         for instrument in mainInstruments:
-            assert instrument in currentInstruments, "Attention! Instrument [{}] not in available list or change it's name!".format(instrument)
+            assert instrument in currentInstruments, "Instrument [{}] not in available list or changed its name!".format(instrument)
