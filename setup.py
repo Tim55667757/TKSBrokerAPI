fb5b9cca2642ff23adea09540de870c9280e7fea
Timur Gilmullin <tim55667757@gmail.com>
2023-01-10 20:21:53 +0300

    Mako was added to setup's `tests_require`

--- setup.py
+++ setup.py
@@ -89,6 +89,7 @@ setup(
         "requests >= 2.25.1",  # Apache-2.0 license
         "pandas >= 1.5.2",  # MIT License
         "openpyxl >= 3.0.10",  # MIT License
+        "Mako >= 1.2.4",  # MIT License
         "python-dateutil >= 2.8.1",  # Apache-2.0 license
         "PriceGenerator >= 1.3.81",  # Apache-2.0 license
     ],
